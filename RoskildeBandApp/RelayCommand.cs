f62b60217ea95fd4bce30e114871cf28bb149a12
Martin Kierkegaard <mark@easj.dk>
2016-11-25 14:23:00 +0100

    Implementeret RelayCommand som beskrevet på side 670-673 i Step By Step.

--- RoskildeBandApp/RelayCommand.cs
+++ RoskildeBandApp/RelayCommand.cs
@@ -4,78 +4,88 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Windows.UI.Xaml;
 
 namespace RoskildeBandApp
 {
+    /// <summary>
+    /// A command whose sole purpose is to relay its functionality 
+    /// to other objects by invoking delegates. 
+    /// The default return value for the CanExecute method is 'true'.
+    /// <see cref="RaiseCanExecuteChanged"/> needs to be called whenever
+    /// <see cref="CanExecute"/> is expected to return a different value.
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        private Action methodToExecute;
+        private Func<bool> methodToDetectCanExecute;
+
+        private DispatcherTimer canExecuteChangedEventTimer = null;
+        
         /// <summary>
-        /// A command whose sole purpose is to relay its functionality 
-        /// to other objects by invoking delegates. 
-        /// The default return value for the CanExecute method is 'true'.
-        /// <see cref="RaiseCanExecuteChanged"/> needs to be called whenever
-        /// <see cref="CanExecute"/> is expected to return a different value.
+        /// Raised when RaiseCanExecuteChanged is called.
         /// </summary>
-        public class RelayCommand : ICommand
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Creates a new command that can always execute.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public RelayCommand(Action execute)
+            : this(execute, null)
         {
-            private Action methodToExecute;
-            private readonly Func<bool> methodToDetectCanExecute;
+        }
 
-            /// <summary>
-            /// Raised when RaiseCanExecuteChanged is called.
-            /// </summary>
-            public event EventHandler CanExecuteChanged;
 
-            /// <summary>
-            /// Creates a new command that can always execute.
-            /// </summary>
-            /// <param name="execute">The execution logic.</param>
-            public RelayCommand(Action execute)
-                : this(execute, null)
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="methodToExecute">The execution logic.</param>
+        /// <param name="methodToDetectCanExecute">The execution status logic.</param>
+        public RelayCommand(Action methodToExecute, Func<bool> methodToDetectCanExecute)
+        {
+            if (methodToExecute == null)
+                throw new ArgumentNullException("execute");
+
+            this.methodToExecute = methodToExecute;
+            this.methodToDetectCanExecute = methodToDetectCanExecute;
+            if(methodToDetectCanExecute != null)
             {
+                this.canExecuteChangedEventTimer.Tick += canExecuteChangedEventTimer_Tick;
+                this.canExecuteChangedEventTimer.Interval = new TimeSpan(0, 0, 1);
+                this.canExecuteChangedEventTimer.Start();
             }
-                
+        }
 
-            /// <summary>
-            /// Creates a new command.
-            /// </summary>
-            /// <param name="methodToExecute">The execution logic.</param>
-            /// <param name="methodToDetectCanExecute">The execution status logic.</param>
-            public RelayCommand(Action methodToExecute, Func<bool> methodToDetectCanExecute)
-            {
-                if (methodToExecute == null)
-                    throw new ArgumentNullException("execute");
-                this.methodToExecute = methodToExecute;
-                this.methodToDetectCanExecute = methodToDetectCanExecute;
-            }
+        /// <summary>
+        /// Determines whether this <see cref="RelayCommand"/> can execute in its current state.
+        /// </summary>
+        /// <param name="parameter">
+        /// Data used by the command. If the command does not require data to be passed, this object can be set to null.
+        /// </param>
+        /// <returns>true if this command can be executed; otherwise, false.</returns>
+        public bool CanExecute(object parameter)
+        {
+            //return true;
+            return methodToDetectCanExecute == null ? true : methodToDetectCanExecute();
+        }
 
-            /// <summary>
-            /// Determines whether this <see cref="RelayCommand"/> can execute in its current state.
-            /// </summary>
-            /// <param name="parameter">
-            /// Data used by the command. If the command does not require data to be passed, this object can be set to null.
-            /// </param>
-            /// <returns>true if this command can be executed; otherwise, false.</returns>
-            public bool CanExecute(object parameter)
-            {
-                //return true;
-                return methodToDetectCanExecute == null ? true : methodToDetectCanExecute();
-            }
-
-            /// <summary>
-            /// Executes the <see cref="RelayCommand"/> on the current command target.
-            /// </summary>
-            /// <param name="parameter">
-            /// Data used by the command. If the command does not require data to be passed, this object can be set to null.
-            /// </param>
-            public void Execute(object parameter)
-            {
-                methodToExecute();
-            }
+        /// <summary>
+        /// Executes the <see cref="RelayCommand"/> on the current command target.
+        /// </summary>
+        /// <param name="parameter">
+        /// Data used by the command. If the command does not require data to be passed, this object can be set to null.
+        /// </param>
+        public void Execute(object parameter)
+        {
+            methodToExecute();
+        }
 
         void canExecuteChangedEventTimer_Tick(object sender, object e)
         {
             if (this.CanExecuteChanged != null)
             {
-                this.CanExecuteChanged(this,EventArgs.Empty);
+                this.CanExecuteChanged(this, EventArgs.Empty);
             }
         }
 
@@ -85,13 +95,13 @@ namespace RoskildeBandApp
         /// method has changed.
         /// </summary>
         public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
             {
-                var handler = CanExecuteChanged;
-                if (handler != null)
-                {
-                    handler(this, EventArgs.Empty);
-                }
+                handler(this, EventArgs.Empty);
             }
         }
     }
+}
 
